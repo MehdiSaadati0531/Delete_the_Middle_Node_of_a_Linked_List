436d580b2d0ba9cf96f242cec1ae01ac0786131c
MehdiSaadati <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-09-01 17:56:42 +0430

    Add files via upload

---
+++ Delete_the_Middle_Node_of_a_Linked_List/Delete_the_Middle_Node_of_a_Linked_List/Implement.cs
@@ -0,0 +1,53 @@
+﻿namespace MergeTwoSortedLists
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class Implement
+    {
+        public int Puzzle(ListNode node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1 + Puzzle(node.next);
+            }
+        }
+        public ListNode DeleteMiddle(ListNode head)
+        {
+            if (head.next == null)
+            {
+                return null;
+            }
+            else if (head.next.next == null)
+            {
+                head.next = null;
+            }
+            else
+            {
+                int countOfNodes = Puzzle(head) / 2;
+                double middle = Math.Round(Convert.ToDouble(countOfNodes));
+
+                var tempNode = head;
+                while (middle > 1)
+                {
+                    tempNode = tempNode.next;
+                    middle--;
+                }
+
+                tempNode.next = tempNode.next.next;
+            }
+            return head;
+        }
+    }
+}
